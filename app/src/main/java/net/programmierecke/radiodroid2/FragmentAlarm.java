98db585548c72f827d689f9904511c0835e2b4d3
segler-alex <segler_alex@web.de>
2016-05-02 00:19:47 +0200

    refined fragment alarm

--- app/src/main/java/net/programmierecke/radiodroid2/FragmentAlarm.java
+++ app/src/main/java/net/programmierecke/radiodroid2/FragmentAlarm.java
@@ -20,15 +20,56 @@ import android.widget.TextView;
 import java.util.Calendar;
 
 public class FragmentAlarm extends Fragment {
+    View view;
+
     public FragmentAlarm() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.layout_alarm, container, false);
+        view = inflater.inflate(R.layout.layout_alarm, container, false);
 
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        SharedPreferences.OnSharedPreferenceChangeListener spChanged = new
+                SharedPreferences.OnSharedPreferenceChangeListener() {
+                    @Override
+                    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences,
+                                                          String key) {
+                        Log.e("a","changed prefs");
+                        UpdateOutput();
+                    }
+                };
+
+        final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+        sharedPref.registerOnSharedPreferenceChangeListener(spChanged);
+
+        SwitchCompat s = (SwitchCompat)view.findViewById(R.id.switch1);
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Log.w("ALARM","new state:"+isChecked);
+
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putBoolean("alarm.enabled",isChecked);
+                editor.commit();
+
+                if (isChecked) {
+                    startAlarm();
+                    DialogFragment newFragment = new TimePickerFragment();
+                    newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
+                }else{
+                    stopAlarm();
+                }
+            }
+        });
+
+        UpdateOutput();
+
+        return view;
+    }
+
+    private void UpdateOutput() {
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+
         String stationId = sharedPref.getString("alarm.id", null);
         String stationName = sharedPref.getString("alarm.name", null);
 
@@ -38,32 +79,23 @@ public class FragmentAlarm extends Fragment {
         TextView tvTime = (TextView)view.findViewById(R.id.textViewTime);
         TextView tvStation = (TextView)view.findViewById(R.id.textViewStationName);
 
-        tvTime.setText(""+timeHour+":"+timeMinutes);
+        tvTime.setText(String.format("%02d:%02d",timeHour,timeMinutes));
         tvStation.setText(stationName);
-
-        SwitchCompat s = (SwitchCompat)view.findViewById(R.id.switch1);
-        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Log.w("ALARM","new state:"+isChecked);
-
-                DialogFragment newFragment = new TimePickerFragment();
-                newFragment.show(getActivity().getSupportFragmentManager(), "timePicker");
-            }
-        });
-
-
-
-        return view;
     }
 
     void startAlarm(){
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+
+        int timeHour = sharedPref.getInt("alarm.timeHour", 0);
+        int timeMinutes = sharedPref.getInt("alarm.timeMinutes", 0);
+
         Intent intent = new Intent(getActivity(), AlarmReceiver.class);
         PendingIntent alarmIntent = PendingIntent.getBroadcast(getActivity(), 0, intent, 0);
         AlarmManager alarmMgr = (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 20);
-        calendar.set(Calendar.MINUTE, 51);
+        calendar.set(Calendar.HOUR_OF_DAY, timeHour);
+        calendar.set(Calendar.MINUTE, timeMinutes);
         alarmMgr.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000 *1, alarmIntent);
     }
 
