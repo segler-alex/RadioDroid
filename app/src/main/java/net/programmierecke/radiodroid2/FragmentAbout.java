d254f2318012fea95986858dfa468275e110ebab
Dominik Stadler <dominik.stadler@gmx.at>
2017-06-18 21:33:40 +0200

    issue #57: add version to about-view
    + from https://github.com/centic9/RadioDroid/commit/c4e75998148541daa97df85ff1c61e01ea764320

--- app/src/main/java/net/programmierecke/radiodroid2/FragmentAbout.java
+++ app/src/main/java/net/programmierecke/radiodroid2/FragmentAbout.java
@@ -1,16 +1,27 @@
 package net.programmierecke.radiodroid2;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class FragmentAbout extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.layout_about,null);
+        View view = inflater.inflate(R.layout.layout_about,null);
+
+        TextView aTextVersion = (TextView) view.findViewById(R.id.about_version);
+        if (aTextVersion != null) {
+            String versionName = BuildConfig.VERSION_NAME;
+            Resources resources = getResources();
+            aTextVersion.setText(resources.getString(R.string.about_version, versionName));
+        }
+
+        return view;
     }
 }
