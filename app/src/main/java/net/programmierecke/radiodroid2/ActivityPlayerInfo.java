4b3ad677832435801b95bb171c1b932f6b460fa8
Dominik Stadler <dominik.stadler@gmx.at>
2017-06-18 22:12:21 +0200

    start work on issue #124: detect wifi-state, but not finished at all yet!

--- app/src/main/java/net/programmierecke/radiodroid2/ActivityPlayerInfo.java
+++ app/src/main/java/net/programmierecke/radiodroid2/ActivityPlayerInfo.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.net.wifi.SupplicantState;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -84,6 +86,31 @@ public class ActivityPlayerInfo extends AppCompatActivity {
 			}
 		};
 		registerReceiver(updateUIReciver,filter);
+
+		// Started some work on Wifi-awareness, but not finished at all, see also
+		// https://stackoverflow.com/questions/9353005/android-wifi-how-to-detect-when-specific-wifi-connection-is-available
+        // https://stackoverflow.com/questions/5888502/how-to-detect-when-wifi-connection-has-been-established-in-android
+		BroadcastReceiver broadcastReceiver = new WifiBroadcastReceiver();
+
+		IntentFilter intentFilter = new IntentFilter();
+		intentFilter.addAction(WifiManager.SUPPLICANT_STATE_CHANGED_ACTION);
+
+		getBaseContext().registerReceiver(broadcastReceiver, intentFilter);
+	}
+
+	public class WifiBroadcastReceiver extends BroadcastReceiver {
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			if (WifiManager.SUPPLICANT_STATE_CHANGED_ACTION .equals(action)) {
+				SupplicantState state = intent.getParcelableExtra(WifiManager.EXTRA_NEW_STATE);
+				if (SupplicantState.isValidState(state)
+						&& state == SupplicantState.COMPLETED) {
+					// TODO: connected!
+				}
+			}
+		}
 	}
 
 	private void InitControls() {
