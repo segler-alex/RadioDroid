d8f6fc155df22e4db71b9678a76a7eaa890e6406
werman <wermaan@gmail.com>
2019-12-25 21:16:18 +0200

    Add preference to continue playback when headset disconnects
    See #610

--- app/src/main/java/net/programmierecke/radiodroid2/service/BecomingNoisyReceiver.java
+++ app/src/main/java/net/programmierecke/radiodroid2/service/BecomingNoisyReceiver.java
@@ -3,14 +3,20 @@ package net.programmierecke.radiodroid2.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.AudioManager;
+
+import androidx.preference.PreferenceManager;
 
 public class BecomingNoisyReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
         if (AudioManager.ACTION_AUDIO_BECOMING_NOISY.equals(intent.getAction())) {
-            PlayerServiceUtil.pause();
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+            if (sharedPref.getBoolean("pause_when_noisy", true)) {
+                PlayerServiceUtil.pause();
+            }
         }
     }
 }
