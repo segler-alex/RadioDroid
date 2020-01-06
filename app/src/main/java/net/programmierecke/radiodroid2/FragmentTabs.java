segler-alex/RadioDroid

commit 6a55b03c3a0e21b1e2cd2dbc0ffa68b1e74ed6f2

Show stations during search as query being typed

app/src/main/java/net/programmierecke/radiodroid2/FragmentTabs.java
@@ -38,8 +38,8 @@ public class FragmentTabs extends Fragment implements IFragmentRefreshable, IFra
 
     private String searchQuery; // Search may be requested before onCreateView so we should wait
 
-    FragmentBase[] fragments = new FragmentBase[9];
-    String[] adresses = new String[]{
+    private Fragment[] fragments = new Fragment[9];
+    private String[] adresses = new String[]{
             itsAdressWWWLocal,
             itsAdressWWWTopClick,
             itsAdressWWWTopVote,
@@ -116,11 +116,11 @@ public class FragmentTabs extends Fragment implements IFragmentRefreshable, IFra
         }
         for (int i = 0; i < fragments.length; i++) {
             if (i < 5)
-                fragments[i] = new FragmentStations();
+                fragments[i] = new FragmentStations(false);
             else if (i < 8)
                 fragments[i] = new FragmentCategories();
             else
-                fragments[i] = new FragmentStations();
+                fragments[i] = new FragmentStations(true);
             Bundle bundle1 = new Bundle();
             bundle1.putString("url", RadioBrowserServerManager.getWebserviceEndpoint(getContext(),adresses[i]));
             fragments[i].setArguments(bundle1);
@@ -150,7 +150,7 @@ public class FragmentTabs extends Fragment implements IFragmentRefreshable, IFra
     public void Search(final String query) {
         if (viewPager != null) {
             viewPager.setCurrentItem(8, false);
-            fragments[8].SetDownloadUrl(query);
+            ((IFragmentSearchable)fragments[8]).Search(query);
         } else {
             searchQuery = query;
         }
@@ -158,8 +158,10 @@ public class FragmentTabs extends Fragment implements IFragmentRefreshable, IFra
 
     @Override
     public void Refresh() {
-        FragmentBase fragment = fragments[viewPager.getCurrentItem()];
-        fragment.DownloadUrl(true);
+        Fragment fragment = fragments[viewPager.getCurrentItem()];
+        if (fragment instanceof FragmentBase) {
+            ((FragmentBase) fragment).DownloadUrl(true);
+        }
     }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {

app/src/main/java/net/programmierecke/radiodroid2/station/FragmentStations.java
@@ -1,8 +1,10 @@
 package net.programmierecke.radiodroid2.station;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,25 +17,38 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import com.google.android.material.button.MaterialButton;
+
+import net.programmierecke.radiodroid2.ActivityMain;
 import net.programmierecke.radiodroid2.BuildConfig;
 import net.programmierecke.radiodroid2.FavouriteManager;
 import net.programmierecke.radiodroid2.FragmentBase;
 import net.programmierecke.radiodroid2.R;
 import net.programmierecke.radiodroid2.RadioDroidApp;
 import net.programmierecke.radiodroid2.Utils;
+import net.programmierecke.radiodroid2.interfaces.IFragmentSearchable;
+import net.programmierecke.radiodroid2.utils.CustomFilter;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class FragmentStations extends FragmentBase {
+public class FragmentStations extends FragmentBase implements IFragmentSearchable {
     private static final String TAG = "FragmentStations";
 
     private RecyclerView rvStations;
+    private ViewGroup layoutError;
+    private MaterialButton btnRetry;
     private SwipeRefreshLayout swipeRefreshLayout;
 
     private SharedPreferences sharedPref;
     private FavouriteManager favouriteManager;
 
-    void onStationClick(DataRadioStation theStation) {
+    private boolean searchEnabled;
+
+    private CustomFilter stationsFilter;
+    private String lastQuery = "";
+
+    void onStationClick(DataRadioStation theStation, int pos) {
         Context context = getContext();
 
         RadioDroidApp radioDroidApp = (RadioDroidApp) getActivity().getApplication();
@@ -45,13 +60,18 @@ public class FragmentStations extends FragmentBase {
             favouriteManager.add(theStation);
             Toast toast = Toast.makeText(context, context.getString(R.string.notify_autostarred), Toast.LENGTH_SHORT);
             toast.show();
-            RefreshListGui();
+
+            rvStations.getAdapter().notifyItemChanged(pos);
         }
+    }
+
+    public FragmentStations(boolean searchEnabled) {
+        this.searchEnabled = searchEnabled;
     }
 
     @Override
     protected void RefreshListGui() {
-        if (rvStations == null) {
+        if (rvStations == null || !hasUrl()) {
             return;
         }
 
@@ -65,9 +85,9 @@ public class FragmentStations extends FragmentBase {
         boolean show_broken = sharedPref.getBoolean("show_broken", false);
 
         ArrayList<DataRadioStation> filteredStationsList = new ArrayList<>();
-        DataRadioStation[] radioStations = DataRadioStation.DecodeJson(getUrlResult());
+        List<DataRadioStation> radioStations = DataRadioStation.DecodeJson(getUrlResult());
 
-        if (BuildConfig.DEBUG) Log.d(TAG, "station count:" + radioStations.length);
+        if (BuildConfig.DEBUG) Log.d(TAG, "station count:" + radioStations.size());
 
         for (DataRadioStation station : radioStations) {
             if (show_broken || station.Working) {
@@ -78,6 +98,9 @@ public class FragmentStations extends FragmentBase {
         ItemAdapterStation adapter = (ItemAdapterStation) rvStations.getAdapter();
         if (adapter != null) {
             adapter.updateList(null, filteredStationsList);
+            if (searchEnabled) {
+                stationsFilter.filter("");
+            }
         }
     }
 
@@ -87,15 +110,17 @@ public class FragmentStations extends FragmentBase {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_stations_remote, container, false);
         rvStations = (RecyclerView) view.findViewById(R.id.recyclerViewStations);
+        layoutError = view.findViewById(R.id.layoutError);
+        btnRetry = view.findViewById(R.id.btnRefresh);
 
         RadioDroidApp radioDroidApp = (RadioDroidApp) getActivity().getApplication();
         favouriteManager = radioDroidApp.getFavouriteManager();
 
-        ItemAdapterStation adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station);
+        ItemAdapterStation adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station, StationsFilter.FilterType.GLOBAL);
         adapter.setStationActionsListener(new ItemAdapterStation.StationActionsListener() {
             @Override
-            public void onStationClick(DataRadioStation station) {
-                FragmentStations.this.onStationClick(station);
+            public void onStationClick(DataRadioStation station, int pos) {
+                FragmentStations.this.onStationClick(station, pos);
             }
 
             @Override
@@ -111,6 +136,36 @@ public class FragmentStations extends FragmentBase {
             }
         });
 
+        if (searchEnabled) {
+            stationsFilter = adapter.getFilter();
+
+            stationsFilter.setDelayer(new CustomFilter.Delayer() {
+                private int previousLength = 0;
+
+                public long getPostingDelay(CharSequence constraint) {
+                    if (constraint == null) {
+                        return 0;
+                    }
+
+                    long delay = 0;
+                    if (constraint.length() < previousLength) {
+                        delay = 500;
+                    }
+                    previousLength = constraint.length();
+
+                    return delay;
+                }
+            });
+
+            adapter.setFilterListener(searchStatus -> {
+                layoutError.setVisibility(searchStatus == StationsFilter.SearchStatus.ERROR ? View.VISIBLE : View.GONE);
+                getContext().sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                swipeRefreshLayout.setRefreshing(false);
+            });
+
+            btnRetry.setOnClickListener(v -> Search(lastQuery));
+        }
+
         LinearLayoutManager llm = new LinearLayoutManager(getContext());
         llm.setOrientation(LinearLayoutManager.VERTICAL);
 
@@ -123,11 +178,11 @@ public class FragmentStations extends FragmentBase {
 
         swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.swiperefresh);
         swipeRefreshLayout.setOnRefreshListener(
-                new SwipeRefreshLayout.OnRefreshListener() {
-                    @Override
-                    public void onRefresh() {
-                        if (BuildConfig.DEBUG) Log.d(TAG, "swipe to refresh.");
+                () -> {
+                    if (hasUrl()) {
                         DownloadUrl(true, false);
+                    } else if (searchEnabled) {
+                        Search(lastQuery);
                     }
                 }
         );
@@ -141,6 +196,18 @@ public class FragmentStations extends FragmentBase {
     public void onDestroyView() {
         super.onDestroyView();
         rvStations.setAdapter(null);
+    }
+
+    @Override
+    public void Search(String query) {
+        if (rvStations != null && searchEnabled) {
+            if (!TextUtils.isEmpty(query)) {
+                getContext().sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+            }
+
+            lastQuery = query;
+            stationsFilter.filter(query);
+        }
     }
 
     @Override

app/build.gradle
@@ -100,6 +100,7 @@ dependencies {
     implementation 'com.mikepenz:community-material-typeface:3.5.95.1-kotlin@aar'
     implementation 'com.github.rustamg:file-dialogs:1.0'
     implementation 'info.debatty:java-string-similarity:1.2.1'
+    implementation 'me.xdrop:fuzzywuzzy:1.2.0'
 
     implementation "androidx.room:room-runtime:2.1.0"
     annotationProcessor "androidx.room:room-compiler:2.1.0"

app/src/main/java/net/programmierecke/radiodroid2/ActivityMain.java
@@ -44,6 +44,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.navigation.NavigationView;
 
+import com.google.android.material.tabs.TabLayout;
 import com.mikepenz.iconics.Iconics;
 import com.mikepenz.iconics.context.IconicsLayoutInflater2;
 import com.rustamg.filedialogs.FileDialog;
@@ -53,6 +54,7 @@ import com.rustamg.filedialogs.SaveFileDialog;
 import net.programmierecke.radiodroid2.alarm.FragmentAlarm;
 import net.programmierecke.radiodroid2.alarm.TimePickerFragment;
 import net.programmierecke.radiodroid2.interfaces.IFragmentRefreshable;
+import net.programmierecke.radiodroid2.interfaces.IFragmentSearchable;
 import net.programmierecke.radiodroid2.service.MediaSessionCallback;
 import net.programmierecke.radiodroid2.service.PlayerServiceUtil;
 import net.programmierecke.radiodroid2.station.DataRadioStation;
@@ -87,6 +89,7 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
     private SearchView mSearchView;
 
     private AppBarLayout appBarLayout;
+    private TabLayout tabsView;
 
     DrawerLayout mDrawerLayout;
     NavigationView mNavigationView;
@@ -160,6 +163,7 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         mFragmentManager = getSupportFragmentManager();
 
         appBarLayout = findViewById(R.id.app_bar_layout);
+        tabsView = findViewById(R.id.tabs);
         mDrawerLayout = findViewById(R.id.drawerLayout);
         mNavigationView = findViewById(R.id.my_navigation_view);
         mBottomNavigationView = findViewById(R.id.bottom_navigation);
@@ -529,6 +533,22 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         menuItemAddAlarm = menu.findItem(R.id.action_add_alarm);
         mSearchView = (SearchView) MenuItemCompat.getActionView(menuItemSearch);
         mSearchView.setOnQueryTextListener(this);
+        mSearchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            private int prevTabsVisibility = View.GONE;
+
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                mBottomNavigationView.setVisibility(hasFocus ? View.GONE : View.VISIBLE);
+
+                if (hasFocus) {
+                    prevTabsVisibility = tabsView.getVisibility();
+                    tabsView.setVisibility(View.GONE);
+                } else {
+                    tabsView.setVisibility(prevTabsVisibility);
+                }
+
+            }
+        });
 
         menuItemSleepTimer.setVisible(false);
         menuItemSearch.setVisible(false);
@@ -821,6 +841,13 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
 
     }
 
+    public void SearchStations(@NonNull String query) {
+        Fragment currentFragment = mFragmentManager.getFragments().get(mFragmentManager.getFragments().size() - 1);
+        if (currentFragment instanceof IFragmentSearchable) {
+            ((IFragmentSearchable) currentFragment).Search(query);
+        }
+    }
+
 //    public void togglePlayer() {
 //        FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
 //        if (smallPlayerFragment.isDetached()) {
@@ -836,23 +863,24 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
 
     @Override
     public boolean onQueryTextSubmit(String query) {
-        String queryEncoded;
-        try {
-            mSearchView.setQuery("", false);
-            mSearchView.clearFocus();
-            mSearchView.setIconified(true);
-            queryEncoded = URLEncoder.encode(query, "utf-8");
-            queryEncoded = queryEncoded.replace("+", "%20");
-            Search(RadioBrowserServerManager.getWebserviceEndpoint(this, "json/stations/byname/" + queryEncoded));
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+//        String queryEncoded;
+//        try {
+//            mSearchView.setQuery("", false);
+//            mSearchView.clearFocus();
+//            mSearchView.setIconified(true);
+//            queryEncoded = URLEncoder.encode(query, "utf-8");
+//            queryEncoded = queryEncoded.replace("+", "%20");
+//            SearchStations(query);
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        }
         return true;
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
-        return false;
+        SearchStations(newText);
+        return true;
     }
 
     private void setupBroadcastReceiver() {

app/src/main/java/net/programmierecke/radiodroid2/FragmentBase.java
@@ -50,6 +50,10 @@ public class FragmentBase extends Fragment {
         return urlResult;
     }
 
+    protected boolean hasUrl() {
+        return !TextUtils.isEmpty(url);
+    }
+
     public void SetDownloadUrl(String theUrl) {
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "new url " + theUrl);

app/src/main/java/net/programmierecke/radiodroid2/FragmentHistory.java
@@ -21,6 +21,7 @@ import com.google.android.material.snackbar.Snackbar;
 import net.programmierecke.radiodroid2.station.ItemAdapterStation;
 import net.programmierecke.radiodroid2.station.DataRadioStation;
 import net.programmierecke.radiodroid2.interfaces.IAdapterRefreshable;
+import net.programmierecke.radiodroid2.station.StationsFilter;
 
 public class FragmentHistory extends Fragment implements IAdapterRefreshable {
     private static final String TAG = "FragmentStarred";
@@ -66,10 +67,10 @@ public class FragmentHistory extends Fragment implements IAdapterRefreshable {
         historyManager = radioDroidApp.getHistoryManager();
         favouriteManager = radioDroidApp.getFavouriteManager();
 
-        ItemAdapterStation adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station);
+        ItemAdapterStation adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station, StationsFilter.FilterType.LOCAL);
         adapter.setStationActionsListener(new ItemAdapterStation.StationActionsListener() {
             @Override
-            public void onStationClick(DataRadioStation station) {
+            public void onStationClick(DataRadioStation station, int pos) {
                 FragmentHistory.this.onStationClick(station);
             }
 

app/src/main/java/net/programmierecke/radiodroid2/FragmentStarred.java
@@ -25,6 +25,7 @@ import net.programmierecke.radiodroid2.station.ItemAdapterStation;
 import net.programmierecke.radiodroid2.station.DataRadioStation;
 import net.programmierecke.radiodroid2.station.ItemAdapterIconOnlyStation;
 import net.programmierecke.radiodroid2.interfaces.IAdapterRefreshable;
+import net.programmierecke.radiodroid2.station.StationsFilter;
 
 import java.util.Observable;
 import java.util.Observer;
@@ -66,7 +67,7 @@ public class FragmentStarred extends Fragment implements IAdapterRefreshable, Ob
         ItemAdapterStation adapter;
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(getContext());
         if (sharedPref.getBoolean("load_icons", false) && sharedPref.getBoolean("icons_only_favorites_style", false)) {
-            adapter = new ItemAdapterIconOnlyStation(getActivity(), R.layout.list_item_icon_only_station);
+            adapter = new ItemAdapterIconOnlyStation(getActivity(), R.layout.list_item_icon_only_station, StationsFilter.FilterType.LOCAL);
             Context ctx = getContext();
             DisplayMetrics displayMetrics = ctx.getResources().getDisplayMetrics();
             int itemWidth = (int) ctx.getResources().getDimension(R.dimen.regular_style_icon_container_width);
@@ -76,7 +77,7 @@ public class FragmentStarred extends Fragment implements IAdapterRefreshable, Ob
             rvStations.setLayoutManager(glm);
             ((ItemAdapterIconOnlyStation)adapter).enableItemMove(rvStations);
         } else {
-            adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station);
+            adapter = new ItemAdapterStation(getActivity(), R.layout.list_item_station, StationsFilter.FilterType.LOCAL);
             LinearLayoutManager llm = new LinearLayoutManager(getContext());
             llm.setOrientation(RecyclerView.VERTICAL);
 
@@ -90,7 +91,7 @@ public class FragmentStarred extends Fragment implements IAdapterRefreshable, Ob
 
         adapter.setStationActionsListener(new ItemAdapterStation.StationActionsListener() {
             @Override
-            public void onStationClick(DataRadioStation station) {
+            public void onStationClick(DataRadioStation station, int pos) {
                 FragmentStarred.this.onStationClick(station);
             }
 

app/src/main/java/net/programmierecke/radiodroid2/StationSaveManager.java
@@ -226,8 +226,8 @@ public class StationSaveManager extends Observable {
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         String str = sharedPref.getString(getSaveId(), null);
         if (str != null) {
-            DataRadioStation[] arr = DataRadioStation.DecodeJson(str);
-            Collections.addAll(listStations, arr);
+            List<DataRadioStation> arr = DataRadioStation.DecodeJson(str);
+            listStations.addAll(arr);
             if (hasInvalidUuids() && Utils.hasAnyConnection(context)) {
                 refreshStationsFromServer();
             }

commit 1be023d0db0a2987509aaa44ffe7e8bb78e4e442

Fix FragmentStations rotation
Fixes: 6a55b03c3a0e21b1e2cd2dbc0ffa68b1e74ed6f2
Closes: #651
Closes: #649

app/src/main/java/net/programmierecke/radiodroid2/FragmentTabs.java
@@ -115,15 +115,19 @@ public class FragmentTabs extends Fragment implements IFragmentRefreshable, IFra
             adresses[0] = "json/stations/bycountryexact/" + country + "?order=clickcount&reverse=true";
         }
         for (int i = 0; i < fragments.length; i++) {
-            if (i < 5)
-                fragments[i] = new FragmentStations(false);
-            else if (i < 8)
+            Bundle bundle = new Bundle();
+
+            if (i < 5) {
+                fragments[i] = new FragmentStations();
+            } else if (i < 8) {
                 fragments[i] = new FragmentCategories();
-            else
-                fragments[i] = new FragmentStations(true);
-            Bundle bundle1 = new Bundle();
-            bundle1.putString("url", RadioBrowserServerManager.getWebserviceEndpoint(getContext(),adresses[i]));
-            fragments[i].setArguments(bundle1);
+            } else {
+                fragments[i] = new FragmentStations();
+                bundle.putBoolean(FragmentStations.KEY_SEARCH_ENABLED, true);
+            }
+
+            bundle.putString("url", RadioBrowserServerManager.getWebserviceEndpoint(getContext(),adresses[i]));
+            fragments[i].setArguments(bundle);
         }
 
         ((FragmentCategories) fragments[5]).EnableSingleUseFilter(true);

app/src/main/java/net/programmierecke/radiodroid2/station/FragmentStations.java
@@ -35,6 +35,8 @@ import java.util.List;
 public class FragmentStations extends FragmentBase implements IFragmentSearchable {
     private static final String TAG = "FragmentStations";
 
+    public static final String KEY_SEARCH_ENABLED = "SEARCH_ENABLED";
+
     private RecyclerView rvStations;
     private ViewGroup layoutError;
     private MaterialButton btnRetry;
@@ -43,7 +45,7 @@ public class FragmentStations extends FragmentBase implements IFragmentSearchabl
     private SharedPreferences sharedPref;
     private FavouriteManager favouriteManager;
 
-    private boolean searchEnabled;
+    private boolean searchEnabled = false;
 
     private CustomFilter stationsFilter;
     private String lastQuery = "";
@@ -63,10 +65,6 @@ public class FragmentStations extends FragmentBase implements IFragmentSearchabl
 
             rvStations.getAdapter().notifyItemChanged(pos);
         }
-    }
-
-    public FragmentStations(boolean searchEnabled) {
-        this.searchEnabled = searchEnabled;
     }
 
     @Override
@@ -107,6 +105,11 @@ public class FragmentStations extends FragmentBase implements IFragmentSearchabl
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            searchEnabled = bundle.getBoolean(KEY_SEARCH_ENABLED, false);
+        }
+
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_stations_remote, container, false);
         rvStations = (RecyclerView) view.findViewById(R.id.recyclerViewStations);