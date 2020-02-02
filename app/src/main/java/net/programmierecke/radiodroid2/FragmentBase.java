segler-alex/RadioDroid

commit 1bad42e5bd63abf14ae1aeabf51af7ef4938441a

Fix and improve off-wifi warning, make player states more user friendly
Off-wifi warning is now more broad metered connection warning.
If metered connection is detected during playback - it would be paused
until user explicitly continues it. Either via popup or pressing play
in notification or external device.

Notable changes:
 - Errors in connectivity/metered warning/player error are displayed
   in notification which in place of playback status. Notification is
   not removed when error occurs.
 - Fix sudden stop of the playback after manually resuming when connection
   is lost.
 - More sane and consistent display of player selection (e.g. with external player)
 - If there was station playing before make possible to resume it even
   if stations history is cleared.

Closes: #641

app/src/main/java/net/programmierecke/radiodroid2/FragmentBase.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 
 import java.util.HashMap;
@@ -104,7 +105,7 @@ public class FragmentBase extends Fragment {
                     protected void onPostExecute(String result) {
                         DownloadFinished();
                         if(getContext() != null)
-                            getContext().sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                            LocalBroadcastManager.getInstance(getContext()).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
                         if (BuildConfig.DEBUG) {
                             Log.d(TAG, "Download relativeUrl finished:" + relativeUrl);
                         }

app/src/main/java/net/programmierecke/radiodroid2/FragmentServerInfo.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import net.programmierecke.radiodroid2.adapters.ItemAdapterStatistics;
 import net.programmierecke.radiodroid2.data.DataStatistics;
@@ -54,7 +55,7 @@ public class FragmentServerInfo extends Fragment implements IFragmentRefreshable
             @Override
             protected void onPostExecute(String result) {
                 if(getContext() != null)
-                    getContext().sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                    LocalBroadcastManager.getInstance(getContext()).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
                 if (result != null) {
                     itemAdapterStatistics.clear();
                     DataStatistics[] items = DataStatistics.DecodeJson(result);

app/src/main/java/net/programmierecke/radiodroid2/StationSaveManager.java
@@ -16,6 +16,7 @@ import net.programmierecke.radiodroid2.station.DataRadioStation;
 import net.programmierecke.radiodroid2.interfaces.IChanged;
 
 import androidx.annotation.Nullable;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import org.json.JSONArray;
 
@@ -217,7 +218,7 @@ public class StationSaveManager extends Observable {
 
                 notifyObservers();
 
-                context.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
                 super.onPostExecute(result);
             }
         }.execute();

app/src/main/java/net/programmierecke/radiodroid2/players/RadioPlayer.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 
 import net.programmierecke.radiodroid2.ActivityMain;
@@ -39,6 +40,8 @@ public class RadioPlayer implements PlayerWrapper.PlayListener, Recordable {
 
     public interface PlayerListener {
         void onStateChanged(final PlayState status, final int audioSessionId);
+
+        void onPlayerWarning(final int messageId);
 
         void onPlayerError(final int messageId);
 
@@ -285,9 +288,14 @@ public class RadioPlayer implements PlayerWrapper.PlayListener, Recordable {
     }
 
     @Override
+    public void onPlayerWarning(int messageId) {
+        playerThreadHandler.post(() -> playerListener.onPlayerWarning(messageId));
+    }
+
+    @Override
     public void onPlayerError(int messageId) {
-        stop();
-        playerListener.onPlayerError(messageId);
+        pause();
+        playerThreadHandler.post(() -> playerListener.onPlayerError(messageId));
     }
 
     @Override
@@ -339,7 +347,7 @@ public class RadioPlayer implements PlayerWrapper.PlayListener, Recordable {
         @Override
         protected void onCancelled() {
             if (radioPlayer.realStationLinkRetrieveTask == null) {
-                radioPlayer.mainContext.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                LocalBroadcastManager.getInstance(radioPlayer.mainContext).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
             }
 
             super.onCancelled();
@@ -347,7 +355,7 @@ public class RadioPlayer implements PlayerWrapper.PlayListener, Recordable {
 
         @Override
         protected void onPostExecute(String result) {
-            radioPlayer.mainContext.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+            LocalBroadcastManager.getInstance(radioPlayer.mainContext).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
 
             if (result != null) {
                 station.playableUrl = result;

app/src/main/java/net/programmierecke/radiodroid2/players/mpd/MPDServersAdapter.java
@@ -17,6 +17,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,6 +31,8 @@ import net.programmierecke.radiodroid2.players.mpd.tasks.MPDChangeVolumeTask;
 import net.programmierecke.radiodroid2.players.mpd.tasks.MPDPauseTask;
 import net.programmierecke.radiodroid2.players.mpd.tasks.MPDPlayTask;
 import net.programmierecke.radiodroid2.players.mpd.tasks.MPDResumeTask;
+import net.programmierecke.radiodroid2.service.ConnectivityChecker;
+import net.programmierecke.radiodroid2.service.PlayerService;
 import net.programmierecke.radiodroid2.service.PlayerServiceUtil;
 import net.programmierecke.radiodroid2.station.DataRadioStation;
 
@@ -144,7 +147,7 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                 return;
             }
 
-            ctx.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+            LocalBroadcastManager.getInstance(ctx).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
 
             if (result != null) {
                 stationToPlay.playableUrl = result;
@@ -179,6 +182,8 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
     private final Context context;
 
     private final boolean showPlayInExternal;
+    private final boolean warnOnMeteredConnection;
+
     private int fixedViewsCount;
 
     private List<Integer> viewTypes = new ArrayList<>();
@@ -201,6 +206,7 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
         SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
         showPlayInExternal = sharedPref.getBoolean("play_external", false) && stationToPlay != null;
+        warnOnMeteredConnection = sharedPref.getBoolean(PlayerService.METERED_CONNECTION_WARNING_KEY, false);
 
         fixedViewsCount = 0;
         if (stationToPlay != null) {
@@ -267,7 +273,7 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                     holder.btnPlay.setImageResource(R.drawable.ic_play_circle);
                     holder.btnPlay.setContentDescription(context.getString(R.string.detail_play));
                 } else {
-                    Utils.Play((RadioDroidApp) context.getApplicationContext(), stationToPlay);
+                    Utils.playAndWarnIfMetered((RadioDroidApp) context.getApplicationContext(), stationToPlay);
 
                     holder.btnPlay.setImageResource(R.drawable.ic_pause_circle);
                     holder.btnPlay.setContentDescription(context.getResources().getString(R.string.detail_pause));
@@ -276,7 +282,14 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         } else if (holder.getItemViewType() == PlayerType.EXTERNAL.getValue()) {
             holder.textViewDescription.setText(R.string.action_play_in_external);
 
-            holder.btnPlay.setOnClickListener(view -> new PlayStationTask(PlayerType.EXTERNAL, null, null, stationToPlay, context).execute());
+            Runnable play = () -> new PlayStationTask(PlayerType.EXTERNAL, null, null, stationToPlay, context).execute();
+
+            if (warnOnMeteredConnection && ConnectivityChecker.getCurrentConnectionType(context) == ConnectivityChecker.ConnectionType.METERED) {
+                Utils.showMeteredConnectionDialog(context, play);
+            } else {
+                holder.btnPlay.setOnClickListener(view -> play.run());
+            }
+
         } else if (holder.getItemViewType() == PlayerType.CAST.getValue()) {
             holder.textViewDescription.setText(R.string.media_route_menu_title);
 

app/src/main/java/net/programmierecke/radiodroid2/station/StationActions.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.FragmentActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import net.programmierecke.radiodroid2.ActivityMain;
 import net.programmierecke.radiodroid2.R;
@@ -104,7 +105,7 @@ public class StationActions {
                     return;
                 }
 
-                ctx.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                LocalBroadcastManager.getInstance(ctx).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
 
                 if (result != null) {
                     ClipboardManager clipboard = (ClipboardManager) ctx.getSystemService(Context.CLIPBOARD_SERVICE);
@@ -171,7 +172,7 @@ public class StationActions {
                     return;
                 }
 
-                ctx.sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+                LocalBroadcastManager.getInstance(ctx).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
 
                 if (result != null) {
                     Intent share = new Intent(Intent.ACTION_VIEW);

app/src/main/java/net/programmierecke/radiodroid2/ActivityMain.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -36,6 +37,7 @@ import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.preference.PreferenceManager;
 
 import com.bytehamster.lib.preferencesearch.SearchPreferenceResult;
@@ -58,6 +60,7 @@ import net.programmierecke.radiodroid2.interfaces.IFragmentRefreshable;
 import net.programmierecke.radiodroid2.interfaces.IFragmentSearchable;
 import net.programmierecke.radiodroid2.players.PlayState;
 import net.programmierecke.radiodroid2.service.MediaSessionCallback;
+import net.programmierecke.radiodroid2.service.PlayerService;
 import net.programmierecke.radiodroid2.service.PlayerServiceUtil;
 import net.programmierecke.radiodroid2.station.DataRadioStation;
 import net.programmierecke.radiodroid2.station.StationsFilter;
@@ -163,7 +166,6 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         setSupportActionBar(myToolbar);
 
         PlayerServiceUtil.bind(this);
-        setupBroadcastReceiver();
 
         selectedMenuItem = sharedPref.getInt("last_selectedMenuItem", -1);
         instanceStateWasSaved = savedInstanceState != null;
@@ -341,7 +343,7 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
             fragmentTransaction.replace(R.id.containerView, f).addToBackStack(backStackTag).commit();
 
         // User selected a menuItem. Let's hide progressBar
-        sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
+        LocalBroadcastManager.getInstance(this).sendBroadcast(new Intent(ActivityMain.ACTION_HIDE_LOADING));
         invalidateOptionsMenu();
         checkMenuItems();
 
@@ -501,12 +503,10 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
     public void onDestroy() {
         super.onDestroy();
         PlayerServiceUtil.unBind(this);
-        unregisterReceiver(broadcastReceiver);
     }
 
     @Override
     protected void onPause() {
-
         SharedPreferences.Editor ed = sharedPref.edit();
         ed.putInt("last_selectedMenuItem", selectedMenuItem);
         ed.apply();
@@ -514,6 +514,8 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "PAUSED");
         }
+
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(broadcastReceiver);
 
         super.onPause();
 
@@ -531,6 +533,8 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         if (BuildConfig.DEBUG) {
             Log.d(TAG, "RESUMED");
         }
+
+        setupBroadcastReceiver();
 
         PlayerServiceUtil.bind(this);
         CastHandler.onResume();
@@ -922,17 +926,22 @@ public class ActivityMain extends AppCompatActivity implements SearchView.OnQuer
         IntentFilter filter = new IntentFilter();
         filter.addAction(ACTION_HIDE_LOADING);
         filter.addAction(ACTION_SHOW_LOADING);
+        filter.addAction(PlayerService.PLAYER_SERVICE_METERED_CONNECTION);
         broadcastReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context context, Intent intent) {
-                Intent mIntent = intent;
-                if (mIntent.getAction().equals(ACTION_HIDE_LOADING))
+                if (intent.getAction().equals(ACTION_HIDE_LOADING)) {
                     hideLoadingIcon();
-                else if (mIntent.getAction().equals(ACTION_SHOW_LOADING))
+                } else if (intent.getAction().equals(ACTION_SHOW_LOADING)) {
                     showLoadingIcon();
+                } else if (intent.getAction().equals(PlayerService.PLAYER_SERVICE_METERED_CONNECTION)) {
+                    Utils.showMeteredConnectionDialog(ActivityMain.this,
+                            () -> Utils.play((RadioDroidApp) getApplication(), PlayerServiceUtil.getCurrentStation()));
+                }
             }
         };
-        registerReceiver(broadcastReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(broadcastReceiver, filter);
     }
 
     // Loading listener

app/src/main/java/net/programmierecke/radiodroid2/FragmentPlayerFull.java
@@ -435,11 +435,15 @@ public class FragmentPlayerFull extends Fragment {
 
     private void playLastFromHistory() {
         RadioDroidApp radioDroidApp = (RadioDroidApp) requireActivity().getApplication();
-        HistoryManager historyManager = radioDroidApp.getHistoryManager();
-        DataRadioStation lastStation = historyManager.getFirst();
+        DataRadioStation station = PlayerServiceUtil.getCurrentStation();
 
-        if (lastStation != null) {
-            Utils.Play(radioDroidApp, lastStation);
+        if (station == null) {
+            HistoryManager historyManager = radioDroidApp.getHistoryManager();
+            station = historyManager.getFirst();
+        }
+
+        if (station != null) {
+            Utils.showPlaySelection(radioDroidApp, station, getActivity().getSupportFragmentManager());
         }
     }
 

commit 932f4b7b42b35c32974ce9660d7b487d7e5ffc7d

Fix ACTION_SHOW_LOADING not being sent via LocalBroadcastManager
Fixes: 1bad42e5bd63abf14ae1aeabf51af7ef4938441a

app/src/main/java/net/programmierecke/radiodroid2/FragmentBase.java
@@ -100,7 +100,7 @@ public class FragmentBase extends Fragment {
             String cache = Utils.getCacheFile(getActivity(), relativeUrl);
             if (cache == null || forceUpdate) {
                 if (getContext() != null && displayProgress) {
-                    getContext().sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+                    LocalBroadcastManager.getInstance(getContext()).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
                 }
 
                 RadioDroidApp radioDroidApp = (RadioDroidApp) getActivity().getApplication();

app/src/main/java/net/programmierecke/radiodroid2/FragmentServerInfo.java
@@ -41,7 +41,7 @@ public class FragmentServerInfo extends Fragment implements IFragmentRefreshable
     }
 
     void Download(final boolean forceUpdate){
-        getContext().sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+        LocalBroadcastManager.getInstance(getContext()).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
 
         RadioDroidApp radioDroidApp = (RadioDroidApp) getActivity().getApplication();
         final OkHttpClient httpClient = radioDroidApp.getHttpClient();

app/src/main/java/net/programmierecke/radiodroid2/StationSaveManager.java
@@ -197,7 +197,7 @@ public class StationSaveManager extends Observable {
     private void refreshStationsFromServer() {
         final RadioDroidApp radioDroidApp = (RadioDroidApp) context.getApplicationContext();
         final OkHttpClient httpClient = radioDroidApp.getHttpClient();
-        context.sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
 
         new AsyncTask<Void, Void, Integer>() {
             @Override

app/src/main/java/net/programmierecke/radiodroid2/players/RadioPlayer.java
@@ -323,7 +323,7 @@ public class RadioPlayer implements PlayerWrapper.PlayListener, Recordable {
 
         @Override
         protected void onPreExecute() {
-            radioPlayer.mainContext.sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+            LocalBroadcastManager.getInstance(radioPlayer.mainContext).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
 
             super.onPreExecute();
         }

app/src/main/java/net/programmierecke/radiodroid2/players/mpd/MPDServersAdapter.java
@@ -125,7 +125,7 @@ public class MPDServersAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
             Context ctx = contextWeakReference.get();
             if (ctx != null) {
-                ctx.sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+                LocalBroadcastManager.getInstance(ctx).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
             }
         }
 

app/src/main/java/net/programmierecke/radiodroid2/station/StationActions.java
@@ -79,7 +79,7 @@ public class StationActions {
     }
 
     private static void retrieveAndCopyStreamUrlToClipboard(final @NonNull Context context, final @NonNull DataRadioStation station) {
-        context.sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
 
         final WeakReference<Context> contextRef = new WeakReference<>(context);
 
@@ -147,7 +147,7 @@ public class StationActions {
     }
 
     public static void share(final @NonNull Context context, final @NonNull DataRadioStation station) {
-        context.sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
+        LocalBroadcastManager.getInstance(context).sendBroadcast(new Intent(ActivityMain.ACTION_SHOW_LOADING));
 
         final WeakReference<Context> contextRef = new WeakReference<>(context);
 