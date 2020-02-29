segler-alex/RadioDroid

commit d6e1a225cf981877c94cf9714da070ce63f5b7aa

ignore all.api server (#740)
Closes: #739

app/src/main/java/net/programmierecke/radiodroid2/RadioBrowserServerManager.java
@@ -30,7 +30,10 @@ public class RadioBrowserServerManager {
             InetAddress[] list = InetAddress.getAllByName("all.api.radio-browser.info");
             for (InetAddress item : list) {
                 Log.i("DNS", "Found: " + item.toString() + " -> " + item.getCanonicalHostName());
-                listResult.add(item.getCanonicalHostName());
+                String name = item.getCanonicalHostName();
+                if (name != "all.api.radio-browser.info") {
+                    listResult.add(item.getCanonicalHostName());
+                }
             }
         } catch (UnknownHostException e) {
             e.printStackTrace();

commit dd568ccf1c7937254436c90b57d47a865b23c413

Fix string comparisons (use .equals)
Fixes: d6e1a225cf981877c94cf9714da070ce63f5b7aa
Fixes: 4aadd2dbc7861e5762ce517331edde2b5e1ae9ea

app/src/main/java/net/programmierecke/radiodroid2/RadioBrowserServerManager.java
@@ -33,7 +33,7 @@ public class RadioBrowserServerManager {
                 InetAddress new_item = InetAddress.getByName(item.getHostAddress());
                 Log.i("DNS", "Found: " + new_item.toString() + " -> " + new_item.getCanonicalHostName());
                 String name = item.getCanonicalHostName();
-                if (name != "all.api.radio-browser.info") {
+                if (!name.equals("all.api.radio-browser.info")) {
                     listResult.add(item.getCanonicalHostName());
                 }
             }

app/src/main/java/net/programmierecke/radiodroid2/Utils.java
@@ -201,7 +201,7 @@ public class Utils {
 
         // try all other servers for download
         for (String newServer : serverList) {
-            if (newServer == currentServer) {
+            if (newServer.equals(currentServer)) {
                 continue;
             }
 