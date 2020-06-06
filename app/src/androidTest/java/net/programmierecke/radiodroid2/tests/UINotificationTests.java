9ed8801d164a6e4f0790684b293e3c81c37b882b
werman <wermaan@gmail.com>
2020-06-06 14:53:35 +0300

    Use ConditionWatcher for testing notifications
    It's all timing based...

--- app/src/androidTest/java/net/programmierecke/radiodroid2/tests/UINotificationTests.java
+++ app/src/androidTest/java/net/programmierecke/radiodroid2/tests/UINotificationTests.java
@@ -2,7 +2,6 @@ package net.programmierecke.radiodroid2.tests;
 
 import android.content.Context;
 import android.media.AudioManager;
-import android.os.SystemClock;
 
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.espresso.ViewInteraction;
@@ -13,6 +12,7 @@ import androidx.test.filters.SdkSuppress;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.BySelector;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.Until;
@@ -20,20 +20,21 @@ import androidx.test.uiautomator.Until;
 import net.programmierecke.radiodroid2.ActivityMain;
 import net.programmierecke.radiodroid2.R;
 import net.programmierecke.radiodroid2.tests.utils.TestUtils;
+import net.programmierecke.radiodroid2.tests.utils.conditionwatcher.ConditionWatcher;
+import net.programmierecke.radiodroid2.tests.utils.conditionwatcher.IsMusicPlayingCondition;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import javax.annotation.Nonnull;
+
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayingAtLeast;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.core.AllOf.allOf;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -54,11 +55,6 @@ public class UINotificationTests {
         return onView(allOf(withId(R.id.buttonPlay), isDisplayingAtLeast(80)));
     }
 
-    private boolean isMusicPlaying() {
-        AudioManager manager = (AudioManager) ApplicationProvider.getApplicationContext().getSystemService(Context.AUDIO_SERVICE);
-        return manager.isMusicActive();
-    }
-
     private void launchPausedNotification() {
         ViewInteraction btnPlay = getPlayButton();
 
@@ -71,6 +67,33 @@ public class UINotificationTests {
         btnPlay.perform(ViewActions.click());
     }
 
+    private static class UIObjectWaiterCondition implements ConditionWatcher.Condition {
+        private UiDevice uiDevice;
+        private BySelector bySelector;
+
+        public UIObjectWaiterCondition(UiDevice uiDevice, BySelector bySelector) {
+            this.uiDevice = uiDevice;
+            this.bySelector = bySelector;
+        }
+
+        @Override
+        public boolean testCondition() {
+            return uiDevice.findObject(bySelector) != null;
+        }
+
+        @Nonnull
+        @Override
+        public String getDescription() {
+            return "Wait for any view to match " + bySelector.toString();
+        }
+    }
+
+    private void waitForObject(UiDevice uiDevice, BySelector bySelector) {
+        ConditionWatcher.waitForCondition(
+                new UIObjectWaiterCondition(uiDevice, bySelector),
+                ConditionWatcher.SHORT_WAIT_POLICY);
+    }
+
     @Test
     public void playback_ShouldStart_OnResumeFromNotification() {
         launchPausedNotification();
@@ -79,18 +102,17 @@ public class UINotificationTests {
         uiDevice.openNotification();
 
         String expectedAppName = ApplicationProvider.getApplicationContext().getString(R.string.app_name);
-        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 100);
+        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 250);
+        uiDevice.wait(Until.hasObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_resume))), 250);
 
         UiObject2 resumeBtn = uiDevice.findObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_resume)));
         assertNotNull(resumeBtn);
 
         resumeBtn.click();
 
-        SystemClock.sleep(200);
+        waitForObject(uiDevice, By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_pause)));
 
-        UiObject2 pauseBtn = uiDevice.findObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_pause)));
-        assertNotNull(pauseBtn);
-        assertTrue(isMusicPlaying());
+        ConditionWatcher.waitForCondition(new IsMusicPlayingCondition(true), ConditionWatcher.SHORT_WAIT_POLICY);
     }
 
     @Test
@@ -101,19 +123,17 @@ public class UINotificationTests {
         uiDevice.openNotification();
 
         String expectedAppName = ApplicationProvider.getApplicationContext().getString(R.string.app_name);
-        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 100);
+        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 250);
+        uiDevice.wait(Until.hasObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_pause))), 250);
 
         UiObject2 pauseBtn = uiDevice.findObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_pause)));
         assertNotNull(pauseBtn);
 
         pauseBtn.click();
 
-        SystemClock.sleep(200);
+        waitForObject(uiDevice, By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_resume)));
 
-        UiObject2 resumeBtn = uiDevice.findObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_resume)));
-        assertNotNull(resumeBtn);
-
-        assertFalse(isMusicPlaying());
+        ConditionWatcher.waitForCondition(new IsMusicPlayingCondition(false), ConditionWatcher.SHORT_WAIT_POLICY);
     }
 
     @Test
@@ -124,14 +144,27 @@ public class UINotificationTests {
         uiDevice.openNotification();
 
         String expectedAppName = ApplicationProvider.getApplicationContext().getString(R.string.app_name);
-        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 100);
+        uiDevice.wait(Until.hasObject(By.textStartsWith(expectedAppName)), 250);
+        uiDevice.wait(Until.hasObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_stop))), 250);
 
         UiObject2 stopBtn = uiDevice.findObject(By.desc(ApplicationProvider.getApplicationContext().getString(R.string.action_stop)));
         assertNotNull(stopBtn);
 
         stopBtn.click();
 
-        assertNull(uiDevice.findObject(By.textStartsWith(expectedAppName)));
-        assertFalse(isMusicPlaying());
+        ConditionWatcher.waitForCondition(new ConditionWatcher.Condition() {
+            @Override
+            public boolean testCondition() {
+                return uiDevice.findObject(By.textStartsWith(expectedAppName)) == null;
+            }
+
+            @Nonnull
+            @Override
+            public String getDescription() {
+                return "Wait for notification to disappear";
+            }
+        }, ConditionWatcher.SHORT_WAIT_POLICY);
+
+        ConditionWatcher.waitForCondition(new IsMusicPlayingCondition(false), ConditionWatcher.SHORT_WAIT_POLICY);
     }
 }
